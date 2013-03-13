79bafbbaa822719adf482b555d02efcb27124734
Johny Jose <johny.aiesec@gmail.com>
2013-03-13 17:38:29 +0530

    Updated example app to use passport-playlyfe & playlyfe-node-sdk

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 express = require('express');
 passport = require('passport');
-playlyfe = require('playlyfe-passport-oauth');
+PlaylyfeOAuth2Strategy = require('passport-playlyfe');
+RedisStore = require('connect-redis')(express);
+Playlyfe = require('playlyfe-node-sdk');
+
+// This must be changed for other applications.
+var config = {
+  clientID: 'YjFmYzg4ZGQtNDI5Yi00ZDY2LWE3OTQtOGM2N2NmMGIwZTFl',
+  clientSecret: 'YzVlYjI2ZmQtYzAwNi00OWZhLTkyYWMtYzNhODdlN2EzMWFhMTc0YzI4YjAtODhlZC0xMWUyLThlMWUtNDE4ZTM0OThkMmRh',
+  redirectURI: 'http://games.playlyfe.com/pomodoro/auth'
+};
 
 app = express();
+app.set('domain', 'games.playlyfe.com');
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.cookieParser());
 app.use(express.session({
-  secret: 'playlyfe'
+  domain: { path: '/pomodoro' },
+  secret: 'playlyfe',
+  store: new RedisStore({ db: 1 })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+// use playlyfe middleware
+
+app.use(new Playlyfe(config).connect());
+
 app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 app.use(express.errorHandler({
@@ -18,11 +35,9 @@ app.use(express.errorHandler({
   showStack: true
 }));
 
-playlyfe.initialize(passport, {
-  clientID: 'YjFmYzg4ZGQtNDI5Yi00ZDY2LWE3OTQtOGM2N2NmMGIwZTFl',
-  clientSecret: 'YzVlYjI2ZmQtYzAwNi00OWZhLTkyYWMtYzNhODdlN2EzMWFhMTc0YzI4YjAtODhlZC0xMWUyLThlMWUtNDE4ZTM0OThkMmRh',
-  redirectURI: 'http://games.playlyfe.com/pomodoro/auth'
-});
+passport.use(new PlaylyfeOAuth2Strategy(config, function(accessToken, refreshToken, profile, done) {
+  done(null, profile);
+}));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
--- controllers/core.js
+++ controllers/core.js
@@ -1,18 +1,46 @@
 passport = require('passport');
-playlyfe = require('playlyfe-passport-oauth');
 
 CoreController = function(app) {
 
-  app.get('/auth', passport.authenticate('playlyfe'), function(req, res, next) {
-    res.redirect('/pomodoro');
-  });
+    app.get('/auth', function(req, res, next) {
+      passport.authenticate('playlyfe', {}, function (err, user, info){
+        if (!user) res.redirect('/pomodoro');
+        if (err) res.redirect('/pomodoro');
+        else next();
+      })(req, res, next);
+    }, function(req, res, next) {
+      res.redirect('/pomodoro');
+    }, function(err, req, res, next) {
+      console.log(err);
+      res.redirect('/pomodoro');
+    });
 
-  app.all('/api/*', playlyfe.proxy());
+    // Proxy requests to playlyfe server.
+    app.all('/api/*', function(req, res) {
+      res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+      res.header("Pragma", "no-cache");
+      res.header("Expires", 0);
+      req.playlyfe.api(
+        req.params[0],
+        req.route.method.toUpperCase(),
+        req.body,
+        function(err, result, response) {
+          if(err) {
+            if(err.error === 'bad_response') {
+              return res.send(err.statusCode, 'An error occured while contacting playlyfe.com');
+            } else {
+              return res.json(err.statusCode, err.data);
+            }
+          }
+          res.json(result);
+        }
+      );
+    });
 
-  app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/pomodoro');
-  });
+    app.get('/logout', function (req, res) {
+      req.logout();
+      req.playlyfe.logout('http://games.playlyfe.com/pomodoro');
+    });
 };
 
 module.exports = CoreController;
